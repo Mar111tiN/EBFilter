edb1bc3a8bd3e5766249b2e1ff64898918fa7be2
Martin Szyska <martinszyska37@gmail.com>
2019-04-10 17:20:20 +0200

    creating setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ebfilter',
+      version='1.0.0',
+      description='Python tools for filtering somatic mutations using beta-binomial sequencing error model.',
+      author='Martin Szyska modified from original EBFilter by Yuichi Shiraishi',
+      author_email='martinszyska37@gmail.com',
+      url='https://github.com/Mar111tiN/EBFilter/',
+      package_dir = {'': 'lib'},
+      packages=['ebfilter'],
+      scripts=['EBrun'],
+      license='GPL-3'
+     )
+
+
