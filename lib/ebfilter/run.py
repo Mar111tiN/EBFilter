6eff14d0302670881f61314d3499219a4d24485b
Martin Szyska <martinszyska37@gmail.com>
2019-04-15 19:05:47 +0200

    updated lib

--- lib/ebfilter/run.py
+++ lib/ebfilter/run.py
@@ -4,6 +4,7 @@ import re
 import os
 import pandas as pd
 import numpy as np
+import math
 import subprocess
 import multiprocessing
 from multiprocessing import Pool
@@ -12,8 +13,7 @@ from functools import partial
 from .utils import validate, read_anno_csv, validate_bam, validate_pon, validate_cache
 from . import anno
 from . import vcf
-from .cache import generate_cache
-
+from .cache import generate_cache, get_EB_from_cache
 
 
 def main(args, config):
@@ -21,130 +21,140 @@ def main(args, config):
     validates files and refers to respective functions
     '''
     
-
     ############### ARGUMENTS and CONFIG ##################################
     config['cache_mode'] = False
-    generate_cache = args['generate_cache'] if 'generate_cache' in args.keys() else False
-    config['cache_folder'] = cache_folder = args['use_cache'] if 'use_cache' in args.keys() else None
-    config['bed_file'] = args['bed_file'] if 'bed_files' in args.keys() else None
+    generate_cache_mode = args['generate_cache'] if 'generate_cache' in args.keys() else False
+
     threads = config['threads']
     debug_mode = config['debug_mode']
+    pon_dict = validate_pon(args['pon_list'])  
 
+    ###### set chromosome ########################### 
+    config['chr'] = args['chromosome'] if 'chromosome' in args.keys() else 'all'
+    if config['chr']:
+        # reduce chromosome list to config['chr'] (unless 'all' or bad name)
+        if config['chr'] in pon_dict['chr_list']:
+            pon_dict['chr_list'] = [config['chr']]
+        elif config['chr'] != 'all':
+            sys.stderr.write('''
+                    Provided chromosome name is not found in bam files! Exiting..
+                    ''')
+            sys.exit(1)
 
     ##################### --> GENERATE CACHE ###################
-    if args['generate_cache']:
-        ###### set cache folder #########################
-        
+    if generate_cache_mode:
+
+        ###### set cache folder #########################    
+        if 'cache_folder' in args.keys():
+            config['cache_folder'] = cache_folder = args['cache_folder']
+        else:
+        # if no cache folder is provided, pon_list folder is used
+            config['cache_folder'] = os.path.join(os.path.dirname(pon_dict['list']), '.cache')
         if not os.path.isdir(cache_folder):
             os.mkdir(cache_folder)
 
-        ###### set bed file #########################
-        if not args['bed_file']:
+
+        ###### set bed file #############################
+        config['bed_file'] = args['bed_file'] if 'bed_file' in args.keys() else None
+        if not config['bed_file']:
             if not args['force_caching']:
                 sys.stderr.write('''
                     Please provide a bed_file or use -force caching option!\n
                     Generating a cache file for the entire genomic stretch covered by the PoN potentially takes forever!!
                     ''')
                 sys.exit(1)
-        pon_list = validate_pon(args['pon_list'])
-        success = generate_cache(pon_list, config)
+        success = generate_cache(pon_dict, config)
         print(success)
         return
 
+    ##################### EBscore ###################
     else: # EBscore mode
-        if config['cache_folder']:
-            cache_folder = validate_cache(config['cache_folder'])
+        cache_folder = args['use_cache'] if 'use_cache' in args.keys() else None
+        if cache_folder:
+            config['cache_folder'] = validate_cache(cache_folder, pon_dict)
             config['cache_mode'] = True
+            print('Running EBscore in Cache mode...')
 
-    pon_list = validate_pon(args['pon_list'])            
+    # returns a pon dict: {'df': pon_df, 'list': pon_list}
+          
         
     # get arguments for EBscore 
-    sep = config['sep']
     mut_file = validate(args['mut_file'], "No target mutation file")
-    print('mutfile: ', mut_file )
+    config['sep'] = args['sep'] if 'sep' in args.keys() else '\t'
+    if not config['sep'] in [',', '\t', ':']:
+        print(f'Separator \" {config["sep"]} \" cannot be used. Trying to open mutation file with separator \" \\t \"..')
+
     tumor_bam = validate_bam(args['tumor_bam'])
     output_path = args['output_path']   
     is_anno = not(os.path.splitext(mut_file)[-1] == '.vcf')
     region = args['region'] if 'region' in args.keys() else ''
-
-    # create log directory - remove in snakemake
-    log_folder = os.path.split(config['log'])[0]
-    if not os.path.exists(log_folder) or os.path.isfile(log_folder):
-        os.makedirs(log_folder)
     
-
+    ##################### EBscore ANNO ###################   
     if is_anno:
+        print(f'Loading annotation file {mut_file}..')
         # create dataframe (maybe more options for other input file formats)
         anno_df, original_columns = read_anno_csv(mut_file, config)
         # create small copy for working with
         mut_df = anno_df[anno_df.columns[:5]].copy()
 
-        if threads == 1:
-        # non multi-threading mode
-            if config['cache_mode']:
-                # do multi-threaded merging of the different AB_files per chromosome
-                #AB_columns = pd.MultiIndex.from_product([['A','C','T','G'],['+', '-'],['a','b']], names=['var', 'strand', 'param'])
-                # read in the AB_df for the different chromosomes or total
-                #AB_df = pd.read_csv('')
+        ############### CACHE MODE ###############
+        EBcached_outputs = []
+        if config['cache_mode']:
+        ################# CACHE RETRIEVAL ####################################
+            # do multi-threaded merging of the different AB_files per chromosome
 
-                out_df = anno.worker(tumor_bam, pon_list, output_path, region, config, mut_df) # -1 means single-threaded
+            # get the SNPs for the annotation
+            snp_df = mut_df.query('not (Ref == "-" or Alt == "-")')
+            # get the valid chromosomes
+            chromosomes = pon_dict['chr_list']
+            # init multicore
+            getEB_pool = Pool(threads)
+            # threads are mapped to the pool of chromosomes
+            print('Drawing BB parameters from cache and piling up the target bam..')
+            mut_EBs = getEB_pool.map(partial(get_EB_from_cache, snp_df, tumor_bam, config), chromosomes)
+            # EBcached_outputs will be concatenated with the annoworker dfs
+            getEB_pool.close()
+            getEB_pool.join()
+            EBcached_outputs = mut_EBs
 
-
-        else: # multi-threading mode
-            if config['cache_mode']:
-                # do multi-threaded merging of the different AB_files per chromosome
-                AB_columns = pd.MultiIndex.from_product([['A','C','T','G'],['+', '-'],['a','b']], names=['var', 'strand', 'param'])
-                # read in the AB_df for the different chromosomes or total
-                AB_df = pd.read_csv('')
-
+        ################# NOT CACHE MODE // INDELS IN CACHE MODE ############### 
+            # reduce mut_df to the remaining indel occurrences for anno worker
+            mut_df = mut_df.query('Ref == "-" or Alt == "-"')
+        # if all are snps, mut_df is now empty after caching and will raise error
+        print(mut_df)
+        mut_len = len(mut_df.index)
+        if mut_len > 0:
             #  setup the processor pool
             anno_pool = Pool(threads)
+            # set the minimum number of lines for one thread to 2000
+            split_factor = min(math.ceil(mut_len / 2000), threads)
+
             # mut_split is the argument pool for anno_pool
-            mut_split = np.array_split(mut_df, threads)
+            mut_split = np.array_split(mut_df, split_factor)
+            print('Piling up target and PoN for EBscore computation')
             # run partial function anno_partial with mut_df as remaining argument to iterate over for multiprocessing
-            out_dfs = anno_pool.map(partial(anno.worker, tumor_bam, pon_list, output_path, region, config), mut_split)  # mut_split is the iterable df_pool
+            out_dfs = anno_pool.map(partial(anno.worker, tumor_bam, pon_dict, output_path, region, config), mut_split)  # mut_split is the iterable df_pool
             anno_pool.close()
             anno_pool.join()
-            out_df = pd.concat(out_dfs)
-            out_df = out_df.sort_values([out_df.columns[0], out_df.columns[1]])
+            # add the output from EBcached to the other output
+            out_dfs += EBcached_outputs
+        else:
+            out_dfs = EBcached_outputs
+        out_df = pd.concat(out_dfs)
+        out_df = out_df.sort_values([out_df.columns[0], out_df.columns[1]])
 
+        # add the original columns back to the annotation file
         final_df = pd.merge(left=anno_df, right=out_df, how='outer', on=['Chr', 'Start'])
         if original_columns is not None:
             final_df.columns = list(original_columns) + ['EB_score']
+        print(f'Writing annotation file {output_path} with EBscores to disc..')
         final_df.to_csv(output_path, sep='\t', index=False)
 
         ################ DEBUG #####################################
         if config['debug_mode']:
             out_file = output_path.replace('eb', "eb_only")
-            out_df.to_csv(out_file, sep=sep, index=False)
-        ############################################################
-
-        
-        
+            out_df.to_csv(out_file, sep=config['sep'], index=False)
+        ############## ##############################################     
+        print('EBscore is finished!')
     else: 
-        if threads == 1:
-            vcf.worker(mut_file, tumor_bam, pon_list, output_path, region, config)
-            # delete intermediate files
-        else:
-            # partition vcf files
-            vcf.partition(mut_file, f"{output_path}.sub.vcf", threads)
-
-            jobs = []
-            for i in range(threads):
-                process = multiprocessing.Process(target = EBFilter_worker_vcf, args = \
-                    (output_path + ".sub.vcf." + str(i), tumor_bam, pon_list, output_path + "." + str(i), region))
-                jobs.append(process)
-                process.start()
-
-            # wait all the jobs to be done
-            for i in range(threads):
-                jobs[i].join()
-
-            # merge the individual results
-            vcf.merge(output_path + ".", output_path, threads)
-
-            # delete intermediate files
-            if debug_mode == False:
-                for i in range(threads):
-                    subprocess.check_call(["rm", output_path + ".sub.vcf." + str(i)])
-                    subprocess.check_call(["rm", output_path + "." + str(i)])
+        print('EBFilter is not YET compatible with .vcf files. ')
--- lib/ebfilter/utils.py
+++ lib/ebfilter/utils.py
@@ -4,6 +4,7 @@ import csv
 import re
 import pandas as pd
 from functools import partial
+import subprocess
 from subprocess import Popen, PIPE, DEVNULL
 from io import StringIO
 
@@ -13,6 +14,23 @@ sign_re = re.compile(r'\^.|\$')
 # regexps for indels with a group for getting indel length
 indel_simple = re.compile(r'[\+\-]([0-9]+)')
 region_simple = re.compile(r"^[^ \t\n\r\f\v,]+:\d+\-\d+")
+
+############# PON2SPLITBAM ############################################
+def split_bam(chromosome, pon_folder, pon_row):
+    '''
+    creates a sub bam file (+bai) of the specified chromosomal region per pon_list row
+    returns the name of the bam file for creating a sub pon_list
+    '''
+
+    bam_file = pon_row[0]
+    # create the name for the sub-bam file as org_bam_name_chr?.bam (and .bai)
+    bam_out = os.path.join(pon_folder, f"{os.path.splitext(os.path.basename(bam_file))[0]}_{str(chromosome)}.bam")
+    split_bam_cmd = ["samtools", "view", "-b", "-o", bam_out, bam_file, str(chromosome)]
+    bam_index_cmd = ["samtools", "index", bam_out]
+    subprocess.check_call(split_bam_cmd)
+    subprocess.check_call(bam_index_cmd)
+    return bam_out
+
 
 def validate_region(region):
     '''
@@ -30,6 +48,7 @@ def sort_chr(dict):
     '''
     sorts all types of chromosome lists
     '''
+
     chr = dict['chr'].replace('Chr', '').replace('chr', '')
     assigner = {'X':50, 'Y':60, 'M':70, '*':80}
     try:
@@ -40,6 +59,28 @@ def sort_chr(dict):
         else:
             chr = 100
     return chr
+
+def bam2chr_list(bam_file):
+    '''
+    creates a list of chromosome names for the input bam
+    '''
+
+
+    bam_stats_cmd = ['samtools', 'idxstats', bam_file]
+    bam_stats = Popen(bam_stats_cmd, stdout=PIPE, stderr=DEVNULL)
+    bam_stats_string = StringIO(bam_stats.communicate()[0].decode('utf-8'))
+    stats_df = pd.read_csv(bam_stats_string, sep='\t', header=None)
+    non_empty = stats_df[stats_df[2] > 100]
+    return list(non_empty[0].T)
+
+
+def pon2chr_list(pon_df):
+    '''
+    generate a chromosome list from the pon_list
+    '''
+    chr_set = set()
+    pon_df[0].apply(lambda bam_file: chr_set.update(bam2chr_list(bam_file)))
+    return list(chr_set)
 
 
 def validate(file, message):
@@ -68,33 +109,43 @@ def validate_pon(pon_list):
     '''
     file existence check for pon_list and the containing bam (and bai) files
     '''
-    validate(pon_list, "No control list file")
-    with open(pon_list) as file_list:
-        for file in file_list:
-            bam_file = file.rstrip()
-            validate_bam(bam_file)
-    return pon_list
+    
+    pon_df = pd.read_csv(validate(pon_list, "No control list file"), header=None)
+    pon_df[0].apply(validate_bam)
+    chr_list = pon2chr_list(pon_df)
+    return {'list': pon_list, 'df': pon_df, 'chr_list': chr_list}
 
 
-def validate_cache(cache_folder, pon_list):
+def validate_cache(cache_folder, pon_dict):
     '''
     file existence check for cache folder and the containing cache files
     '''
+
     if not os.path.isdir(cache_folder):
         sys.stderr.write(f"Cache folder {cache_folder} cannot be found! Exiting..")
         sys.exit(1)        
-    chr_set = Set()
-    with open(pon_list) as file_list:
-        for file in file_list:
-            bam_file = file.rstrip()
-            chr_set.update(bam_to_chr_list(bam_file))
-    cache_files = [os.path.join(cache_folder, f"{chrom}.cache") for chrom in chr_set]
-    cache_files.append("all.cache")
-    for cache_file in cache_files:
-        if not os.path.isfile(cache_folder):
-            file_name = os.path.basename(cache_file)
-            sys.stderr.write(f"Cache file {file_name} cannot be found in folder {cache_folder}! Exiting..")
-            sys.exit(1)  
+    chr_set = pon_dict['chr_list']
+    cache_file_tuples = [(os.path.join(cache_folder, f"{chrom}.cache"), chrom) for chrom in chr_set]
+    all_cache_file = os.path.join(cache_folder, 'all.cache')
+    missing_cache_tuples = []
+    for cache_file_tuple in cache_file_tuples:
+        if not os.path.isfile(cache_file_tuple[0]):
+            if not os.path.isfile(all_cache_file):
+                file_name = os.path.basename(cache_file_tuple[0])
+                sys.stderr.write(f"Cache file {file_name} cannot be found in folder {cache_folder}! Exiting..")
+                sys.exit(1)
+            else:
+                missing_cache_tuples.append(cache_file_tuple)
+    # get chr.cache files from all.cache
+    if len(missing_cache_tuples):
+        all_cache_df = pd.read_csv(all_cache_file, sep=',').set_index('Chr')
+        for t in missing_cache_tuples:
+            try:
+                chr_df = all_cache_df.loc[t[1]].reset_index()
+                all_cache_df.to_csv(t[0], sep=',')
+            except KeyError:
+                print(f'Cache has no data for Chr {t[1]}')
+
     return cache_folder
 
 
@@ -104,6 +155,7 @@ def read_anno_csv(mut_file, config):
     --> returns the dataframe and the original header names
     if no header is detected, the relevant files are names as needed and additional columns are named other1, other2,...
     '''
+
     def to_int(Chr_name):
         '''
         converts all number chromosomes to int
@@ -113,31 +165,34 @@ def read_anno_csv(mut_file, config):
         except ValueError:
             return Chr_name       
 
+    def check_columns(mut_file, anno_df, config):
+        '''
+        raises error, if only one column was detected
+        '''
+        row, col = anno_df.shape
+        if col == 1:
+            sys.stderr.write(f"Only one column detected in {mut_file} - I am guessing wrong separator ( {config['sep']} )?")
+            sys.exit(1)
 
     with open(mut_file, 'r') as input_file:
         has_header = csv.Sniffer().has_header(input_file.read(1024))
 
     sep = config['sep']
-    with open(config['log'],'w+') as log:
-        print(f'Loading annotation file {mut_file} into dataframe', file=log)
 
-
-        if has_header:
-            print(f'Header detected', file=log)
-            anno_df = pd.read_csv(mut_file, sep=sep, converters={0:to_int, 1:to_int, 2:to_int})
-            org_columns = anno_df.columns
-            anno_df.columns = ['Chr','Start','End','Ref', 'Alt'] + list(anno_df.columns[5:])
-        else:
-            anno_df = pd.read_csv(mut_file, sep=sep, header=None, converters={0:to_int, 1:to_int, 2:to_int})
-            row, col = anno_df.shape
-            print(row, col)
-            if col == 1:
-                sys.stderr.write(f"Only one column detected in {mut_file} - I am guessing wrong separator ( {config['sep']} )?")
-                sys.exit(1)
-            org_columns = None
-            rest_columns = [f'other{i+1}' for i in range(len(anno_df.columns) - 5)]
-            anno_df.columns = ['Chr','Start','End','Ref', 'Alt'] + rest_columns
-            print(f'Adding header: Chr\tStart\tEnd\tRef\tAlt\tother1...', file=log)
+    if has_header:
+        print(f'Header detected')
+        anno_df = pd.read_csv(mut_file, sep=sep, converters={0:to_int, 1:to_int, 2:to_int})
+        org_columns = anno_df.columns
+        check_columns(mut_file, anno_df, config)
+        anno_df.columns = ['Chr','Start','End','Ref', 'Alt'] + list(anno_df.columns[5:])
+    else:
+        anno_df = pd.read_csv(mut_file, sep=sep, header=None, converters={0:to_int, 1:to_int, 2:to_int})
+        check_columns(mut_file, anno_df, config)
+        org_columns = None
+        rest_columns = [f'other{i+1}' for i in range(len(anno_df.columns) - 5)]
+        anno_df.columns = ['Chr','Start','End','Ref', 'Alt'] + rest_columns
+    if config['chr'] != 'all':
+        anno_df = anno_df.loc[anno_df['Chr'] == to_int(config['chr'])]
     return (anno_df.sort_values(['Chr', 'Start']), org_columns)
 
 
@@ -161,11 +216,12 @@ def clean_read_column(read_series):
         '''
         removes per column all reads with start/end signs
         '''
-        # should include indel removal as well?
 
+        # should include indel removal as well?
         return read_series.str.replace(sign_re, '')
 
-def clean_up_df(mut_df, pon_count):
+
+def clean_up_df(mut_df, pon_count, config):
     '''
     removes indels and start/end signs from pileup data
     '''
@@ -205,9 +261,6 @@ def clean_up_df(mut_df, pon_count):
             row[f"read{i}"] = remove_indels(row[f"read{i}"], indel_length)
         return row
 
-    # apply partial clean_indels to remove indel traces in pileup
-    mut_df[is_indel] = mut_df[is_indel].apply(partial(clean_indels, pon_count), axis=1)
-    # in case there are indels only in the control files
     def clean_this_row(i, row):
         read = row[f"read{i}"]
         m = indel_simple.search(read)
@@ -217,14 +270,20 @@ def clean_up_df(mut_df, pon_count):
             row[f"read{i}"] = remove_indels(row[f"read{i}"], length)
         return row
 
-    for i in range(pon_count):        
-        read = f"read{i+1}"
-        Q = f"Q{i+1}"
-        # boolean mask for non-fitting read-Q-pairs
-        not_paired = mut_df[read].str.len() != mut_df[Q].str.len()
-        wrong_rows = mut_df[not_paired]
-        if len(wrong_rows.index):
-            mut_df[not_paired] = wrong_rows.apply(partial(clean_this_row, i+1), axis=1)
+    # only clean up other columns if pon pileup is used (non cache_mode)
+    if not config['cache_mode']:
+    # apply partial clean_indels to remove indel traces in pileup
+        mut_df[is_indel] = mut_df[is_indel].apply(partial(clean_indels, pon_count), axis=1)
+    # in case there are indels only in the control files
+
+        for i in range(pon_count):        
+            read = f"read{i+1}"
+            Q = f"Q{i+1}"
+            # boolean mask for non-fitting read-Q-pairs
+            not_paired = mut_df[read].str.len() != mut_df[Q].str.len()
+            wrong_rows = mut_df[not_paired]
+            if len(wrong_rows.index):
+                mut_df[not_paired] = wrong_rows.apply(partial(clean_this_row, i+1), axis=1)
     return mut_df
 
 
@@ -261,15 +320,3 @@ def cleanup_badQ(mut_df, pon_count, filters):
         mut_df[is_snp & has_badQ] = bad_df.apply(partial(remove_badQ, i), axis=1)
 
     return mut_df
-
-
-def bam_to_chr_list(bam_file):
-    '''
-    creates a list of chromosome names for the input bam
-    '''
-
-    bam_stats_cmd = ['samtools', 'idxstats', bam_file]
-    bam_stats = Popen(bam_stats_cmd, stdout=PIPE, stderr=DEVNULL)
-    bam_stats_string = StringIO(bam_stats.communicate()[0].decode('utf-8'))
-    bam_stats_df = pd.read_csv(bam_stats_string, sep='\t', header=None)
-    return list(bam_stats_df[0].T)
