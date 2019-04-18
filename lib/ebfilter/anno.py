f61ae7e7e5d3ea50d6bb7c8e7a973bd4935c56f9
Martin Szyska <martinszyska37@gmail.com>
2019-04-18 08:35:21 +0200

    debugging

--- lib/ebfilter/anno.py
+++ lib/ebfilter/anno.py
@@ -4,13 +4,13 @@ import subprocess
 from subprocess import Popen, PIPE
 from io import StringIO
 from functools import partial
-from .utils import make_region_list, clean_up_df, cleanup_badQ
+from . import utils
 import re
 from .eb import get_EB_score
 
 def worker(tumor_bam, pon_dict, output_path, region, config, mut_df):
 
-    pon_count = 0 if config['cache_mode'] else len(pon_dict['df'].index)
+    pon_count = len(pon_dict['df'].index)
 
     ########### PANDAS IMPORT ################
     # mut_pd = pd.read_csv(mutfile, sep=',')
@@ -18,10 +18,10 @@ def worker(tumor_bam, pon_dict, output_path, region, config, mut_df):
     mut_df = anno2pileup(mut_df, output_path, tumor_bam, pon_dict, region, config)
 
     # in_place removal of indel traces and start/end signs in pileup data
-    clean_up_df(mut_df, pon_count, config)
+    utils.clean_up_df(mut_df, pon_count, config)
 
     # cleanup_badQ should not be necessary because these bases have been removed using mpileup -Q option (?)  
-    #cleanup_badQ(mut_df, pon_count, config['filter_quals'])
+    #utils.cleanup_badQ(mut_df, pon_count, config['filter_quals'])
 
     ############# FOR DEBUGGING #######################
     if config['debug_mode']:
@@ -40,7 +40,7 @@ def anno2pileup(mut_df, out_path, bam, pon_dict, region, config):
     '''
 
     # make region list for use in l_option of mpileup (sets position offset)
-    bed_file = make_region_list(mut_df, out_path, config['threads']) # in utils --> out_1.region_list.bed
+    bed_file = utils.make_region_list(mut_df, out_path, config['threads']) # in utils --> out_1.region_list.bed
 
     # determine wether it is bam or pon      
     mpileup_cmd = ["samtools", "mpileup", "-B", "-d", "10000000", "-q",str(config['q']), "-Q",str(config['Q']), "--ff",config['ff'], "-l", bed_file]
